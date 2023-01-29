75b8e06215ddd4d5daa827e6301c2794ed96457c
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2023-01-29 16:43:36 +0100

    [BUGFIX] Check index before use.

--- Classes/Middleware/Uncache.php
+++ Classes/Middleware/Uncache.php
@@ -23,18 +23,18 @@ class Uncache implements MiddlewareInterface
     {
         if (Environment::getContext()->isDevelopment()) {
             foreach ($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'] as $configurationName => $configuration) {
-                if (is_a(
-                    $configuration['backend'],
-                    \TYPO3\CMS\Core\Cache\Backend\PhpCapableBackendInterface::class,
-                    true
-                )) {
+                if (isset($configuration['backend']) && is_a(
+                        $configuration['backend'],
+                        \TYPO3\CMS\Core\Cache\Backend\PhpCapableBackendInterface::class,
+                        true
+                    )) {
                     $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$configurationName]['backend']
                         = \TYPO3\CMS\Core\Cache\Backend\NullBackend::class;
-                } elseif (is_a(
-                    $configuration['frontend'],
-                    \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
-                    true
-                )) {
+                } elseif (isset($configuration['frontend']) && is_a(
+                        $configuration['frontend'],
+                        \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
+                        true
+                    )) {
                     $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$configurationName]['backend']
                         = \TYPO3\CMS\Core\Cache\Backend\TransientMemoryBackend::class;
                     unset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$configurationName]['options']);
